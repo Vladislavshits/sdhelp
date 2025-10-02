144ec298efad700b8c60c0e5eac0441013c7c745
Vladislavshits <vladislavshits@gmail.com>
2025-10-02 22:47:08 +0300

    Экран "SteamOS"
    Эта вкладка в будущем позволит исправлять какие либо проблемные моменты в SteamOS

---
+++ app/screens/steamos_screen.py
@@ -0,0 +1,23 @@
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QFont
+
+
+class SteamOSScreen(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        layout = QVBoxLayout()
+
+        title = QLabel("Настройки SteamOS")
+        title.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        title_font = QFont()
+        title_font.setPointSize(20)
+        title_font.setBold(True)
+        title.setFont(title_font)
+        title.setStyleSheet("color: white;")
+
+        layout.addWidget(title)
+        self.setLayout(layout)
