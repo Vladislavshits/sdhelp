29bfd95b3560311633cd2afe6d8e97b0e791cbe7
Vladislavshits <vladislavshits@gmail.com>
2025-10-05 18:52:48 +0300

    Delete app/tools directory

--- app/tools/Programms/decky-loader.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rm -f /tmp/user_install_script.sh; if curl -S -s -L -O --output-dir /tmp/ --connect-timeout 60 https://github.com/SteamDeckHomebrew/decky-installer/releases/latest/download/user_install_script.sh; then bash /tmp/user_install_script.sh; else echo "Something went wrong, please report this if it is a bug"; read; fi
