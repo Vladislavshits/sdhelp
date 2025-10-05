8d56852ab98208af9adcbd7c60643b0bb7276275
Vladislavshits <vladislavshits@gmail.com>
2025-10-05 20:11:36 +0300

    версия программы 0.1.0

--- app/config.py
+++ app/config.py
@@ -10,7 +10,7 @@ class Config:
     def load_config(self) -> Dict[str, Any]:
         """Загрузка конфигурации"""
         default_config = {
-            "version": "1.0.0",
+            "version": "0.1.0",
             "functions": {},
             "games": {},
             "programs": {}
